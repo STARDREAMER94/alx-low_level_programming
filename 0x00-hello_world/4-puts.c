909e3462b0407455ef9baca3060d4f9a055c0540
root <onthatile.institute@gmail.com>
2023-03-01 19:45:00 -0800

    prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,9 @@
 
 /**
  * main - Entry point
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * Write a C program that prints exactly:
+ * "Programming is like building a multilingual puzzle
+ * followed by a new line
  * Use the function puts
  * You are not allowed to use printf
  * Your program should end with the value 0
@@ -10,7 +12,8 @@
  */
 int main(void)
 {
-	char string1[80] = '"Programming is like building a multilingual puzzle\n';
+	char string1[80] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(string1);
 	return (0);
 }
