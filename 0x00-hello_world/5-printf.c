8782814478229cd12e22083370776e200bf9f963
root <onthatile.institute@gmail.com>
2023-03-01 19:54:55 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Use the function printf
+ * You are not allowed to use the function puts
+ * compile without warning when using the -Wall gcc option
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
