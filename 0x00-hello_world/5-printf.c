d6b9d89ca9263b3687dc48bb205183f971945d7b
root <onthatile.institute@gmail.com>
2023-03-01 19:58:27 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,9 +7,8 @@
  * compile without warning when using the -Wall gcc option
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,");
 	return (0);
 }
