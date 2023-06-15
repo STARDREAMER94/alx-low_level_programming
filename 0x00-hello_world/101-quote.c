de978ac362aa79f4c51776af82adeda12712edfb
Onthatile <onthatile.institute@gmail.com>
2023-06-15 16:24:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * NOT allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
+ * compile without warning when using the -Wall gcc option
+ * Return: 1 (success)
+ */
+
+int main(void)
+{
+
+   print("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+return(1);
+
+}
