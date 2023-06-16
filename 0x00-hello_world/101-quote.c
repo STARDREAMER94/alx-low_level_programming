9105532d4cce4a99424b657a8a6d325478ec57e9
Onthatile <onthatile.institute@gmail.com>
2023-06-16 08:28:01 -0700

    Printing C program to standard error. 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
+ * A C program that prints a line to the standard error
  * NOT allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
  * compile without warning when using the -Wall gcc option
  * Return: 1 (success)
@@ -10,7 +12,8 @@
 int main(void)
 {
 
-   print("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+     fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59, 1, stderr);
+
 
 return(1);
 
