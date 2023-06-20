1fd8124527ec96a331fdf68d32f79122349ba17e
Onthatile <onthatile.institute@gmail.com>
2023-06-20 15:54:45 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,6 +5,7 @@
  * main - entry point, prints to the standard output
  * a program that prints all single digit numbers of base 10 starting from 0,
  * followed by a new line.
+ * You are not allowed to use any variable of type char
  * ONLY use the putchar function
  * (every other function (printf, puts, etc…) is forbidden)
  * You can only use putchar twice in your code
@@ -17,12 +18,12 @@ int main(void)
 {
 
     /* Get the character to be written */
-	char letter = '0';
+	int letter = 0;
 
     /* Write the Character to stdout */
-	while (letter <= '9')
+	while (letter <= 9)
 	{
-		putchar(letter);
+		putchar('0' + letter);
 		letter++;
 	}
 
