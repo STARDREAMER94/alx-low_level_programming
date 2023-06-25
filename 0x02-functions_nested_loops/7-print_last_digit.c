e0534f23b5c3e6a7bde7117031469127d08dcdd3
Onthatile <onthatile.institute@gmail.com>
2023-06-25 11:20:04 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - a function that prints the last digit of a number
+ * @i: integer observed
+ * Returns the value of the last digit
+ * Return: Always 0 (success) in this case, integer variable n
+ */
+
+int print_last_digit(int i)
+{
+	int n;
+
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+
+	n = i % 10;
+
+	return (n);
+}
