98efd138068d27deb8e81032fd09176c01ec63fe
Onthatile <onthatile.institute@gmail.com>
2023-07-03 15:33:24 -0700

    factor in the leap years

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,6 +22,12 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
+	else if (!((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0)) &&
+		(day == 59 && month == 2))
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
+	}
 	else
 	{
 		if (month == 2 && (day > 60 || day < 59))
@@ -35,7 +41,7 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 			}
-			else
+			else if (day == 59 && month == 2)
 			{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
