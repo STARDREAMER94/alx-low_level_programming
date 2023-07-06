79bd8f9b7829cdd4e1dd46911d7e5382612e9ecc
Onthatile <onthatile.institute@gmail.com>
2023-07-05 19:10:01 -0700

    change value to 98 through pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: inputted pointer value
+ * Return: Void, no return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
