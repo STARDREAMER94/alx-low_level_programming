dd68b2adb1fea784a0a51f748cd2ca189e45ad58
Onthatile <onthatile.institute@gmail.com>
2023-07-06 18:54:58 -0700

    print a string of text to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout
+ * FYI: The standard library provides a similar function: puts
+ * @str - inputted pointer/array variable
+ * Return: void, none
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
