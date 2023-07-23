079859da4104ed825c41711faae4fd35294cfeae
Onthatile <onthatile.institute@gmail.com>
2023-07-22 22:26:01 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * string_nconcat - a function that concatenates two strings
- * The returned pointer shall point to a newly allocated space in memory,
- *   containing s1, followed by the first n bytes of s2, and null terminated
- * If n is greater or equal to the length of s2 then use the entire string s2
- * if NULL is passed, treat it as an empty string
  * @s1: string 1
  * @s2: string 2
  * @n: integer variable
@@ -13,7 +10,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int k = 0, j, l, i = 0;
+	unsigned int k = 0, j = 0, l = 0, i = 0, b = 0, m = 0;
 	char *str;
 
 	if (s1 == NULL)
@@ -22,6 +19,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	while (s1[i])
 		i++;
+	while (s2[m])
+		m++;
+	if (n >= m)
+		b = i + k;
+	else
+		b = i + n;
+
 	str = malloc((sizeof(char) * (i + n)) + 1);
 	if (str == NULL)
 		return (NULL);
@@ -29,11 +33,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str[j] = s1[j];
 	}
-	for (l = j; l <= (j + n); l++)
+	for (l = j; l <= b; l++)
 	{
 		str[l] = s2[k];
 		k++;
 	}
-	str[l] = '\0';
+	str[(l + 1)] = '\0';
 	return (str);
 }
