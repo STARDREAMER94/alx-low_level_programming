7ca16c65049f02870c149cf388e4a6ee041d8671
Onthatile <onthatile.institute@gmail.com>
2023-07-24 12:18:29 -0700

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -22,7 +22,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name);
-		printf("Age: %.6f\n", d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
 	}
 }
