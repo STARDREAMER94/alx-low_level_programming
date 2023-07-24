5be0c9cc637007693410e06bbdb3d931a400bdcf
Onthatile <onthatile.institute@gmail.com>
2023-07-24 10:39:29 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: char pointer
+ * @owner: char pointer
+ * @age: float variable
+ * Description: None
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
