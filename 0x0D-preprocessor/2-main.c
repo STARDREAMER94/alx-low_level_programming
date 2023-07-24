2d6c1f742ef280d56a2c8bd9ac16fa19468af4ab
Onthatile <onthatile.institute@gmail.com>
2023-07-24 00:57:31 -0700

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * prints file name
+ * Write a program that prints the name of the file it was compiled from,
+ * followed by a new line.
+ * You are allowed to use the standard library
+ * Return: Always 0
+ */
+
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
