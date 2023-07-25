fc01d845a337aa21c19f987159f405fec02bb4d2
Onthatile <onthatile.institute@gmail.com>
2023-07-25 00:21:52 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - a function that prints a name
+ * @name: char pointer
+ * f: pointer to function that takes char pointer input
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && F != NULL)
+	{
+		f(name);
+	}
+}
+
