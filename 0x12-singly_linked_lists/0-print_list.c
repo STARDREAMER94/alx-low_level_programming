4d89958e72ef9870de37610f173330398730a9eb
Onthatile <onthatile.institute@gmail.com>
2023-07-27 16:48:26 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list_t list
+ * @h: node structure declaration
+ * Return: the number of nodes
+ * If str is NULL, print [0] (nil)
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+
+	while (h != NULL)
+	{
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+C-programming Introduction
+0x12. C - Singly linked lists
