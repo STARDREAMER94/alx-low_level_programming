d17ee3b0e94d0f0b899f751016a8ca087ad97952
Onthatile <onthatile.institute@gmail.com>
2023-07-27 17:48:02 -0700

    2. Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list
+ * Return: the address of the new element, or NULL if it failed
+ * @str: needs to be duplicated
+ * You are allowed to use strdup
+ * @head: pointer of pointer
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *addnew;
+	unsigned int length = 0;
+
+	while (str[length])
+	{
+		length++;
+	}
+
+	addnew = malloc(sizeof(list_t *));
+
+	if (addnew == NULL)
+		return (NULL);
+
+	addnew->str = strdup(str);
+	addnew->length = length;
+	addnew->next = (*head);
+	(*head) = addnew;
+
+	return (*head);
+}
+
+
+
