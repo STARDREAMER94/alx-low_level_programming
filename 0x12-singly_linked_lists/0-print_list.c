68e765ab73f9e7823beee5364ce6e49e77126154
Onthatile <onthatile.institute@gmail.com>
2023-07-28 05:11:51 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,14 +14,13 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 
-	if (!h->str)
+	if (h->str == NULL)
 		printf("[0] (nil)\n");
 	else
-	{
 		printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		n++;
-	}
+
+	h = h->next;
+	n++;
 	}
 	return (n);
 
