d04dcdaac9178164e319b48d144b9a1c9161461e
Onthatile <onthatile.institute@gmail.com>
2023-07-28 05:17:06 -0700

    fix betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - a function that returns the number of elements in a linked list_t list
+ * list_len - function returning number of elements in a linked list_t list
  * @h: node structure declaration
  * Return: size_t variable that is a counter
  * If str is NULL, print [0] (nil)
