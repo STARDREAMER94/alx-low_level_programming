8fdbb46325ac67169bc4c3eeb33e4ec427581391
Onthatile <onthatile.institute@gmail.com>
2023-07-27 17:27:57 -0700

    0. Print list corrections and 1. List length

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,6 +11,9 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
+	while (h)
+	{
+
 	if (h->str == NULL)
 		printf("[0] (nil)\n");
 	else
@@ -19,7 +22,7 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		n++;
 	}
-
+	}
 	return (n);
 
 }
