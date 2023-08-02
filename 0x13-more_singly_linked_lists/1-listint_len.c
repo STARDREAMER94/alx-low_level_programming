a9c1851c4685c3f5badbb7c8bd09108f88e5bd4d
Onthatile <onthatile.institute@gmail.com>
2023-08-01 20:09:41 -0700

    1. List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 
 /**
- * print_listint - a function that prints all the elements of a listint_t list
+ * listint_len - a function that returns the number of elements in a linked listint_t list
  * @h: pointer to struct node
  * Return: size_t (unsigned int)
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t k;
 
 	k = 0;
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		k++;
 		h = h->next;
 	}
